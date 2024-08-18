c01c449a57277df45388e3d47917369dc5dfa31a
tuannguyen1302 <tuanhienkun13102002@gmail.com>
2024-08-19 01:35:18 +0700

    feact report_screen

--- lib/presentation/features/report/widgets/report_chart_view.dart
+++ lib/presentation/features/report/widgets/report_chart_view.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
 
-import '../../../../data/data_sources/local/db/db_client.dart';
-import '../../../../data/models/api/api_category.dart';
 import '../../../../data/models/category_type.dart';
-import '../../../../data/models/db/db_category.dart';
-import '../../../../utilities/extensions/color_extension.dart';
 import '../../../../utilities/extensions/int_extension.dart';
 import '../../../../utilities/extensions/num_extension.dart';
 import '../../../../utilities/extensions/widget_ref_extension.dart';
@@ -31,26 +26,26 @@ class ReportChartView extends ConsumerWidget {
           Container(
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.width *2/3,
-            color: ref.colors.cellBackground,
-            child: SfCircularChart(
-              series: <CircularSeries>[
-                DoughnutSeries<ChartData, String>(
-                  dataSource: chartDataList,
-                  pointColorMapper: (data, _) =>
-                      HexColor.fromHex(data.dbCategory.color),
-                  xValueMapper: (data, _) => data.dbCategory.getName(ref),
-                  yValueMapper: (data, _) => data.percent,
-                  dataLabelMapper: (data, _) => data.percent.toPercent(),
-                  dataLabelSettings: DataLabelSettings(
-                    isVisible: true,
-                    labelPosition: ChartDataLabelPosition.outside,
-                    textStyle: TextStyle(color: ref.colors.mainText),
-                  ),
-                ),
-              ],
-              tooltipBehavior: _buildTooltipBehavior(ref),
-              legend: _buildLegend(ref),
-            ),
+            // color: ref.colors.cellBackground,
+            // child: SfCircularChart(
+            //   series: <CircularSeries>[
+            //     DoughnutSeries<ChartData, String>(
+            //       dataSource: chartDataList,
+            //       pointColorMapper: (data, _) =>
+            //           HexColor.fromHex(data.dbCategory.color),
+            //       xValueMapper: (data, _) => data.dbCategory.getName(ref),
+            //       yValueMapper: (data, _) => data.percent,
+            //       dataLabelMapper: (data, _) => data.percent.toPercent(),
+            //       dataLabelSettings: DataLabelSettings(
+            //         isVisible: true,
+            //         labelPosition: ChartDataLabelPosition.outside,
+            //         textStyle: TextStyle(color: ref.colors.mainText),
+            //       ),
+            //     ),
+            //   ],
+            //   tooltipBehavior: _buildTooltipBehavior(ref),
+            //   legend: _buildLegend(ref),
+            // ),
           ),
           Container(
             height: 0.5,
@@ -69,59 +64,59 @@ class ReportChartView extends ConsumerWidget {
         ],
       ),
     );
-  return Container();
-  }
-  Legend _buildLegend(WidgetRef ref) {
-    return Legend(
-      isVisible: true,
-      position: LegendPosition.bottom,
-      overflowMode: LegendItemOverflowMode.wrap,
-      textStyle: TextStyle(color: ref.colors.mainText),
-      toggleSeriesVisibility: false,
-    );
-  }
 
-  TooltipBehavior _buildTooltipBehavior(WidgetRef ref) {
-    return TooltipBehavior(
-      enable: true,
-      builder: (data, point, _, __, ___) {
-        final chartData = data as ChartData;
-        return Container(
-          decoration: BoxDecoration(
-            color: ref.colors.tint,
-          ),
-          padding: const EdgeInsets.all(8),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                point.x,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 10,
-                ),
-              ),
-              Text(
-                chartData.totalAmount.toThousandFormatAmount(withUnit: true),
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                point.y?.toPercent() ?? '',
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 10,
-                ),
-              ),
-            ],
-          ),
-        );
-      },
-    );
   }
+  // Legend _buildLegend(WidgetRef ref) {
+  //   return Legend(
+  //     isVisible: true,
+  //     position: LegendPosition.bottom,
+  //     overflowMode: LegendItemOverflowMode.wrap,
+  //     textStyle: TextStyle(color: ref.colors.mainText),
+  //     toggleSeriesVisibility: false,
+  //   );
+  // }
+  //
+  // TooltipBehavior _buildTooltipBehavior(WidgetRef ref) {
+  //   return TooltipBehavior(
+  //     enable: true,
+  //     builder: (data, point, _, __, ___) {
+  //       final chartData = data as ChartData;
+  //       return Container(
+  //         decoration: BoxDecoration(
+  //           color: ref.colors.tint,
+  //         ),
+  //         padding: const EdgeInsets.all(8),
+  //         child: Column(
+  //           mainAxisSize: MainAxisSize.min,
+  //           children: [
+  //             Text(
+  //               point.x,
+  //               style: const TextStyle(
+  //                 color: Colors.white,
+  //                 fontSize: 10,
+  //               ),
+  //             ),
+  //             Text(
+  //               chartData.totalAmount.toThousandFormatAmount(withUnit: true),
+  //               style: const TextStyle(
+  //                 color: Colors.white,
+  //                 fontSize: 14,
+  //                 fontWeight: FontWeight.bold,
+  //               ),
+  //             ),
+  //             Text(
+  //               point.y?.toPercent() ?? '',
+  //               style: const TextStyle(
+  //                 color: Colors.white,
+  //                 fontSize: 10,
+  //               ),
+  //             ),
+  //           ],
+  //         ),
+  //       );
+  //     },
+  //   );
+  // }
   List<ChartData> _getChartData({
     required WidgetRef ref,
   }) {
